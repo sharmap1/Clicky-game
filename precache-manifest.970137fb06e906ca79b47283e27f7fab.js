d99eb38e5a642d22e7e9e948c95cb6f0d0e244b6
Prasamsha Sharma <prasamshari@gmail.com>
2019-12-14 23:23:38 -0800

    Updates

---
+++ precache-manifest.970137fb06e906ca79b47283e27f7fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ab8cc5ee89c7dc1e1ef13f5d36be68",
+    "url": "/Clicky-game/./index.html"
+  },
+  {
+    "revision": "26809bd02583ff68a8fa",
+    "url": "/Clicky-game/./static/css/main.67009cd4.chunk.css"
+  },
+  {
+    "revision": "70c2c66fb1a6aefe6939",
+    "url": "/Clicky-game/./static/js/2.fa63ac40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-game/./static/js/2.fa63ac40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26809bd02583ff68a8fa",
+    "url": "/Clicky-game/./static/js/main.477ceb43.chunk.js"
+  },
+  {
+    "revision": "ad7e0074542793cefde3",
+    "url": "/Clicky-game/./static/js/runtime-main.47d1098d.js"
+  }
+]);
